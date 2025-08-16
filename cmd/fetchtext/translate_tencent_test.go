705dabdd7f6042642681e7ad54a313818119ca95
Hank Shen <swh@admpub.com>
2025-08-16 17:53:02 +0800

    update

--- cmd/fetchtext/translate_tencent_test.go
+++ cmd/fetchtext/translate_tencent_test.go
@@ -8,8 +8,11 @@ import (
 
 func TestTencent(t *testing.T) {
 	lang = `zh-cn`
+	translator = `tencent`
 	translatorConfig = `appid=&secret=`
-	parseTranslatorConfig()
+	err := parseTranslatorConfig()
+	assert.NoError(t, err)
+	t.Logf(`config: %+v`, translatorParsedConfig)
 	text, err := tencentTranslate(`测试`, `en`)
 	assert.Equal(t, nil, err)
 	assert.Equal(t, `test`, text)
