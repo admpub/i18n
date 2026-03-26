d2d67da31c288beb3912ebb061b84528a45d9499
Hank Shen <swhbox@foxmail.com>
2026-03-26 11:29:39 +0800

    update

--- cmd/fetchtext/main_test.go
+++ cmd/fetchtext/main_test.go
@@ -19,7 +19,7 @@ func TestRegexp(t *testing.T) {
 		t.Log(test)
 		assert.True(t, reFunc1.MatchString(test))
 	}
-	for _, test := range []string{`.NewError(code.InvalidParameter, "test"`} {
+	for _, test := range []string{`.NewError(code.InvalidParameter, "test")`} {
 		t.Log(test)
 		assert.True(t, reFunc1_0.MatchString(test))
 	}
@@ -38,5 +38,22 @@ func TestRegexp(t *testing.T) {
 	for _, test := range []string{"{{`text`|$.T}}", "{{`text`|$.T|ToHTML}}"} {
 		t.Log(test)
 		assert.True(t, reTplFunc1_0.MatchString(test))
+	}
+	for _, test := range []string{"{{`{{`}}$ads := {{$tag}} `{{`广告位标识`|$.T}}`{{`}}`}}"} {
+		t.Log(test)
+		assert.Equal(t, `广告位标识`, reTplFunc1_0.FindAllStringSubmatch(test, -1)[0][1])
+		assert.True(t, reTplFunc1_0.MatchString(test))
+	}
+	for _, test := range []string{`{{"te\"xt"|$.T}}`} {
+		t.Log(test)
+		assert.Equal(t, `te\"xt`, reTplFunc1.FindAllStringSubmatch(test, -1)[0][1])
+		t.Log(reTplFunc1.FindAllStringSubmatch(test, -1))
+		assert.True(t, reTplFunc1.MatchString(test))
+	}
+	for _, test := range []string{`{{$.T "te\"xt"}}`} {
+		t.Log(test)
+		t.Log(reTplFunc.FindAllStringSubmatch(test, -1))
+		assert.Equal(t, `te\"xt`, reTplFunc.FindAllStringSubmatch(test, -1)[0][1])
+		assert.True(t, reTplFunc.MatchString(test))
 	}
 }
